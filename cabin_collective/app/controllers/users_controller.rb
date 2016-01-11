28357c6321a6eed82a752ebc039e0cc92dbb8f63
Angus Innes <Angus@Angus-Inness-MacBook-Pro-4.local>
2016-01-11 15:18:14 +0000

    authentication,sessioning and cookies working

--- cabin_collective/app/controllers/users_controller.rb
+++ cabin_collective/app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
-  def create
+ 
+  def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
-  def index
+  def create
+    #binding.pry
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to users_path
+    else 
+      render 'new'
+    end
   end
 
   def show
@@ -19,4 +29,10 @@ class UsersController < ApplicationController
 
   def update
   end
+end
+
+
+private
+  def user_params
+  params.require(:user).permit(:email, :password, :password_confirmation)
 end
--- cabin_collective/config/routes.rb
+++ cabin_collective/config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
 
+  get 'secret/public_info'
+
+  get 'secret/secret'
+
 resources :posts
 
-resources :users 
+resources :users, only: [:new, :index, :create]
 
+get 'login', to: 'sessions#new'
+resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
