3e9877a81dda223e92a7bbcb073a22f427e4ef7b
Angus Innes <Angus@Angus-Inness-MacBook-Pro-4.local>
2016-01-11 09:31:10 +0000

    transferred files into repo

---
+++ cabin_collective/app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def index
+  end
+end
---
+++ cabin_collective/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def update
+  end
+end
